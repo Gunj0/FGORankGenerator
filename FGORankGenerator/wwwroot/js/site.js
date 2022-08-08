83b64afc6c311df513db209de8e48fbdcca72c43
gunjo <gunjo2012@gmail.com>
2022-08-08 20:12:53 +0900

    タイトル文言変更、範囲と色表示、アドセンス追加、所持チェッカー追加

--- FGORankGenerator/wwwroot/js/site.js
+++ FGORankGenerator/wwwroot/js/site.js
@@ -1,4 +1,48 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿'use strict';
 
-// Write your JavaScript code.
+// ローカルストレージからキーの値を取得
+function getLocalStorage(key) {
+  try {
+    return localStorage.getItem(key);
+  }
+  catch (e) {
+    return "error";
+  }
+}
+
+// ローカルストレージにキーと値をセット
+function setLocalStorage(key, value) {
+  try {
+    localStorage.setItem(key, value);
+    return true;
+  }
+  catch (e) {
+    return "error";
+  }
+}
+
+// ローカルストレージからチェック状態を復元
+for (let i = 1; i < 500; i++) {
+  let check = getLocalStorage(i);
+  if (check == 1) {
+    $('#' + i).toggleClass("bg-warning");
+  }
+}
+
+// クリック時動作
+$(document).ready(function () {
+  $('.data').click(function () {
+    // 黄色に変更
+    $(this).toggleClass("bg-warning");
+    // ローカルストレージにチェック保存
+    let id = $(this).attr("id");
+    let check = getLocalStorage(id);
+    if (check != "error") {
+      if (check == "1") {
+        localStorage.setItem(id, '0');
+      } else {
+        localStorage.setItem(id, '1');
+      }
+    }
+  });
+});
